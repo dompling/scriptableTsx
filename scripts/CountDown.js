13dfacddcf61fccb1cfeb58e999669a6721e34e8
dompling <374779789@qq.com>
2020-12-29 15:07:14 +0800

    update: 优化大组件问题

--- scripts/CountDown.js
+++ scripts/CountDown.js
@@ -9,7 +9,7 @@
  * github: https://github.com/dompling/Scriptable
  */
 
-// @编译时间 1609225094619
+// @编译时间 1609225578625
 const MODULE = module;
 let __topLevelAwait__ = () => Promise.resolve();
 function EndAwait(promiseFunc) {
